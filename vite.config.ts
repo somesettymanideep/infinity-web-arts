1cac12af0e471e632394a3e18cfd5c6128b150df
somesetty manideep <manideep723@gmail.com>
2025-11-26 10:46:49 +0530

    dd

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/infinity-web-arts/" : "/",
+  // base: mode === "production" ? "/infinity-web-arts/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
